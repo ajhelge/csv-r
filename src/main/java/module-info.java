863af459905948ebd827600d5d5ff3f23a1daddf
Ahelge <143036477+ajhelge@users.noreply.github.com>
2026-05-01 21:11:00 -0500

    Added opencsv Dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.openjfx.csv_r {
     requires javafx.controls;
     requires javafx.fxml;
+	requires opencsv;
 
     opens org.openjfx.csv_r to javafx.fxml;
     exports org.openjfx.csv_r;
